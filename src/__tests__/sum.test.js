790ab12b471355f0e7659554df8b36ef3841181c
Facundo Flores <flores.facundogabriel@gmail.com>
2019-09-30 14:53:56 -0300

    Test

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -4,4 +4,8 @@ describe('Sum test', () => {
     it('should return 4 when a and b are 2', () => {
         expect(sum(2, 2)).toBe(4);
     })
+
+    it('should return 5 when 3 sum 2', () => {
+        expect(sum(3, 2)).toBe(5);
+    })
 })
